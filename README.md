e8d0f87543193cfc8bef1fe2faa7b851643c73a4
Mikhail <mikhail.sapuzhak@gmail.com>
2020-03-12 17:36:31 +0200

    solution

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# app_login_js
+# App login
+
+Create an authorization form for the application with validation of the entered data in the fields.
+
+- [DEMO-LINK](https://mikhail-88.github.io/app_login_js/dist/index.html)
---
+++ src/js/views/notifications.js
@@ -0,0 +1,58 @@
+function getContainer() {
+  return document.querySelector('.notify-container');
+}
+
+function alertTemplate(msg, className, index) {
+  return `
+    <div class="alert ${className}" data-index="${index}">${msg}</div>
+  `;
+}
+
+function getAlertIndex() {
+  return document.querySelectorAll('.notify-container .alert').length;
+}
+
+function notifyContainerTemplate() {
+  return `
+    <div class="notify-container" style="position: fixed; top: 20px; right: 20px; z-index: 9;"></div>
+  `;
+}
+
+function creatNotifyContainer() {
+  const template = notifyContainerTemplate();
+  document.body.insertAdjacentHTML('afterbegin', template);
+}
+
+function notify({ msg = 'Info message', className = 'alert-info', timeout = 2000} = {}) {
+  if (!getContainer()) {
+    creatNotifyContainer();
+  }
+
+  const index = getAlertIndex();
+  const template = alertTemplate(msg, className, index);
+  const container = getContainer();
+
+  container.insertAdjacentHTML('beforeend', template);
+
+  setTimeout(() => closeNotify(index), timeout);
+}
+
+function closeNotify(index) {
+  let alert;
+
+  if (index === undefined) {
+    alert = document.querySelector('.notify-container .alert');
+  } else {
+    alert = document.querySelector(`.notify-container .alert[data-index="${index}"]`);
+  }
+
+  if (!alert) {
+    console.warn('Alert not found');
+    return;
+  }
+
+  const container = getContainer();
+  container.removeChild(alert);
+}
+
+export default notify;
